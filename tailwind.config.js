18f4489bb9b393f22fe09eee5a2e302b0aef07a5
mahmudfarsi <mhfarsi002022@gmail.com>
2023-10-15 16:15:44 +0330

    add font and screen-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'smm':' 300px'
+      }
+    },
+    screens:{
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily:{
+      raleway:['Raleway', 'sans-serif']
+    }
   },
   plugins: [],
 }
