7bb1a83b1a2ee85ff7c07eb145eb0330e5fff7b8
mahmudfarsi <mhfarsi002022@gmail.com>
2023-10-15 16:25:10 +0330

    add variables in js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const $ = document;
+// modal
+const modal = $.querySelector('.modal');
+const close = $.querySelector('.fa-xmark');
+const input = $.querySelector('#input');
+const btnAdd = $.querySelector('.btn_send');
+
+
+//list
+
+const btnTodo = $.querySelector('.btn_add');
+let boxs = $.querySelectorAll('.box');
